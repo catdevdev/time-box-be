33f1631bf670ba6afa0f0367e9297f5b1b0dad10
Vladislav Oliinyk <catdev.dev@gmail.com>
2021-10-05 19:03:52 +0300

    added

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,19 @@ import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 
 @Module({
   imports: [
     CatsModule,
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
-    MongooseModule.forRoot('mongodb://localhost/nest'),
+    MongooseModule.forRoot(
+      'mongodb+srv://cat:4311067vladvk@cluster0.4vsh8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    ),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -2,13 +2,13 @@ import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Cat, CatDocument } from './schemas/cat.schema';
-import { CreateCatDto } from './dto/create-cat.dto';
+import { CatInput } from './inputs/cat.input';
 
 @Injectable()
 export class CatsService {
   constructor(@InjectModel(Cat.name) private catModel: Model<CatDocument>) {}
 
-  async create(createCatDto: any): Promise<Cat> {
+  async create(createCatDto: CatInput): Promise<Cat> {
     const createdCat = new this.catModel(createCatDto);
     return createdCat.save();
   }
