495d6d2e2892b58aef05ecf7de161260321e1f45
Vladislav Oliinyk <catdev.dev@gmail.com>
2021-10-05 12:20:07 +0300

    learn

---
+++ src/cats/cats.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Cat, CatSchema } from './cat.schema';
+import { CatsResolver } from './cats.resolver';
+import { CatsService } from './cats.service';
+
+@Module({
+  imports: [MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }])],
+  providers: [CatsResolver, CatsService],
+})
+export class CatsModule {}
