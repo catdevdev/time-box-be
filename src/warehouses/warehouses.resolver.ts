42b2d5267eafa51484806b9e413c6b7527f27010
Vladislav Oliinyk <catdev.dev@gmail.com>
2021-10-12 00:23:37 +0300

    done

--- src/warehouses/warehouses.resolver.ts
+++ src/warehouses/warehouses.resolver.ts
@@ -1,16 +1,27 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver, Subscription } from '@nestjs/graphql';
 import { WarehouseType } from './dto/warehouse.dto';
 import { WarehouseInput } from './inputs/warehouse.input';
 import { WarehousesService } from './warehouses.service';
+
+import { PubSub } from 'graphql-subscriptions';
+// import { Subscription } from 'rxjs';
+
+type Vertor3 = { x: number; y: number; z: number };
+
+let Transport = {};
+
+const pubSub = new PubSub();
+
+const positionsBoxes = [
+  {
+    position: { x: 0, y: 0, z: 0 },
+  },
+];
 
 @Resolver()
 export class WarehousesResolver {
   constructor(private warehousesService: WarehousesService) {}
 
-  @Query(() => String)
-  async hello() {
-    return 'Hello 45!';
-  }
   @Query(() => [WarehouseType])
   async warehouses() {
     return this.warehousesService.findAll();
@@ -23,6 +34,13 @@ export class WarehousesResolver {
 
   @Mutation(() => WarehouseType)
   async createdWarehouse(@Args('input') input: WarehouseInput) {
+    // pubSub.publish()
+
     return this.warehousesService.create(input);
+  }
+
+  @Subscription(() => Number)
+  async warehoused() {
+    return this.warehousesService.findAll();
   }
 }
